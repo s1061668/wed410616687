482b5092411af5dc39c9fddf4b190c60b5ec96ee
王韋勛 <open00096@gmail.com>
2021-05-06 20:24:27 +0800

    HW1

---
+++ Web410616687/Controllers/testController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web410616687.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: tses
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+    }
+}
